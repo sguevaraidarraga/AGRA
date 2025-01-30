1d11434f82cf8a9decba532f43687dcf382c3cca
Santiago Guevara <sguevaraidarraga@gmail.com>
2025-01-30 13:48:51 -0500

    changes

--- Tarea 1/factorization.cpp
+++ Tarea 1/factorization.cpp
@@ -13,32 +13,34 @@
 using namespace std;
 
 struct Triple {
-    int num;
+    int num, it;
     vector<int> factors;
     Triple(int n) {
         num = n;
+        it = 2;
         factors = vector<int>();
     }
     Triple(int n, vector<int> &v) {
         num = n;
+        it = v.empty() ? 2 : v[v.size()-1];
         factors = v;
     }
 };
 vector<vector<int>> factorize(int k) {
-    int n, i = 2;
+    int n, *i;
     stack<Triple> s;
     vector<vector<int>> ans;
     s.push(Triple(k));
     Triple *t;
     while(!s.empty()) {
-        t = &s.top(), n = t->num;
-        if(i < n/i) {
-            if(n%i == 0) {
+        t = &s.top(), n = t->num, i = &t->it;
+        if((*i) * (*i) <= n) {
+            if(n%(*i) == 0) {
                 vector<int> aux = s.top().factors;
-                aux.push_back(i);
-                s.push(Triple(i/n, aux));
+                aux.push_back(*i);
+                s.push(Triple(n/(*i), aux));
             }
-            i++;
+            (*i)++;
         }
         else {
             if(k != n) {
@@ -53,7 +55,10 @@ vector<vector<int>> factorize(int k) {
 void printMat(const vector<vector<int>> &m) {
     for(int i = 0; i < m.size(); i++) {
         for(int j = 0; j < m[i].size(); j++) {
-            cout << m[i][j] << ' ';
+            cout << m[i][j];
+            if(j != m[i].size()-1) {
+                cout << ' ';
+            }
         }
         cout << endl;
     }
