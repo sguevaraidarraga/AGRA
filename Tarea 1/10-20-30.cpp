2b57ea0e71372ac7e061426fe27faf85efdb0895
Santiago Guevara <sguevaraidarraga@gmail.com>
2025-02-01 17:40:33 -0500

    changes

---
+++ Tarea 1/10-20-30.cpp
@@ -0,0 +1,17 @@
+/*
+    Arboles y Grafos 2025-1
+    Santiago Guevara Idarraga
+    Problem A - 10-20-30
+    Enero 31 de 2025
+*/
+
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+#define debug(x) cout << #x << ": " << x << endl
+
+int main() {
+    return 0;
+}
