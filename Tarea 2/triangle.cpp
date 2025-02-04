bffee95e708d8fcbae22b302e45e632d2190fd17
Santiago Guevara <sguevaraidarraga@gmail.com>
2025-02-04 14:59:18 -0500

    changes

---
+++ Tarea 2/triangle.cpp
@@ -0,0 +1,19 @@
+/*
+    Arboles y Grafos 2025-1
+    Santiago Guevara Idarraga
+    Problem B: triangle
+    Febrero 4 de 2025
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T, AB, AC, BC, rADE;
+    cin >> T;
+    while(T--) {
+
+    }
+    return 0;
+}
