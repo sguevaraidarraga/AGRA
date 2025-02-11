f0d9850c556552a2179af5610f46949bcad6e908
Santiago Guevara <sguevaraidarraga@gmail.com>
2025-02-10 21:28:16 -0500

    solved

--- Tarea 2/triangle.py
+++ Tarea 2/triangle.py
@@ -5,12 +5,12 @@
     Febrero 10 de 2025
 """
 
+from sys import stdin
 from math import sqrt
-from os import stdin
 
-def solve(AB, ratio):
-    low, high = 0, AB
-    c = AB*sqrt(ratio/(1+ratio))
+def solve(ab, ratio):
+    low, high = 0, ab
+    c = ab*sqrt(ratio/(1+ratio))
     while high-low > 10**-10:
         mid = (low+high)/2
         if mid < c:
@@ -19,6 +19,11 @@ def solve(AB, ratio):
             high = mid
     return mid
 def main():
-    k = 1
-    print(f"Case {k}: {round(solve(100, 2), 7)}")
+    cases = int(stdin.readline())
+    currCase = 1
+    while cases:
+        ab, ac, bc, ratio = map(float, stdin.readline().split())
+        print(f"Case {currCase}: {solve(ab, ratio):.6f}")
+        currCase += 1
+        cases -= 1
 main()
