af732f6953533bc9408325cc6ba9a3654929bb2c
Santiago Guevara <sguevaraidarraga@gmail.com>
2025-02-11 08:26:59 -0500

    changes

--- Tarea 2/bit.cpp
+++ Tarea 2/bit.cpp
@@ -24,45 +24,67 @@
 
 using namespace std;
 
-void printBM(vector<vector<char>> &bm) {
-    for(int i = 0; i < bm.size(); i++) {
+#define debug(x) cout << #x << ": " << x << endl
+
+void printBM(vector<vector<int>> &bm, int l1, int h1, int l2, int h2) {
+    for(int i = l1; i <= h1; i++) {
         cout << '[';
-        for(int j = 0; j < bm[i].size(); j++) {
+        for(int j = l2; j <= h2; j++) {
             cout << bm[i][j];
-            if(j != bm[i].size()-1) {
+            if(j != h2) {
                 cout << ", ";
             }
         }
         cout << ']' << endl;
     }
 }
-void BtoD(vector<vector<char>> &bm, int n1, int n2, int m1, int m2) {
-    int mid1 = (n1+n2)/2, mid2 = (m1+m2)/2;
-    cout << "rows: " << mid1 << endl;
-    cout << "columns: " << mid2 << endl;
-    cout << (mid1 != 1 && mid2 != 1) << endl;
-    if(mid1 != 1 && mid2 != 1) {
-        BtoD(bm, 0, mid1, 0, mid2);
-        BtoD(bm, mid1, n2, mid2, m2);
-        BtoD(bm, 0, mid1, 0, mid2);
-        BtoD(bm, mid1, n2, mid2, m2);
+int BtoD(vector<vector<int>> &bm, int low1, int high1, int low2, int high2, int depth) {
+    int ans, mid1 = (low1+high1)/2, mid2 = (low2+high2)/2, v1, v2, v3, v4;
+    bool f = false;
+    debug(low1), debug(mid1), debug(high1);
+    debug(low2), debug(mid2), debug(high2);
+    printBM(bm, low1, high1, low2, high2);
+    debug((low1 == high1 && low2 == high2));
+    if(low1 == high1 && low2 == high2) {
+        ans = bm[low1][low2];
+        debug(ans);
+        f = true;
     }
+    if(!f) {
+        v1 = BtoD(bm, low1, mid1, low2, mid2, depth+1);
+        v2 = BtoD(bm, low1, mid1, mid2+1, high2, depth+1);
+        v3 = BtoD(bm, mid1+1, high1, low2, mid2, depth+1);
+        v4 = BtoD(bm, mid1+1, high1, mid2+1, high2, depth+1);
+        cout << "Divisiones: "<< v1 << v2 << v3 << v4 << endl;
+        if(depth > 0) {
+            cout << "  => D";
+        }
+        if(v1 == v2 == v3 == v4) {
+            cout << v1;
+        }
+        else {
+            cout << v1 << v2 << v3 << v4;
+        }
+        cout << endl;
+    }
+    return ans;
 }
 int main() {
     char v, c;
-    int rows, columns;
+    int v2, rows, columns;
     while(cin >> c && c != '#') {
         cin >> rows >> columns;
         if(c == 'B') {
-            vector<vector<char>> bitmap(rows, vector<char>(columns));
+            vector<vector<int>> bitmap(rows, vector<int>(columns));
             for(int i = 0; i < bitmap.size(); i++) {
                 for(int j = 0; j < bitmap[i].size(); j++) {
                     cin >> v;
-                    bitmap[i][j] = v;
+                    v2 = v - '0';
+                    bitmap[i][j] = v2;
                 }
             }
-            printBM(bitmap);
-            BtoD(bitmap, 0, rows, 0, columns);
+            //printBM(bitmap);
+            BtoD(bitmap, 0, rows-1, 0, columns-1, 0);
         }
         else if(c == 'D') {
 
