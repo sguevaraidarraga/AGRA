d3e8eedf3fe907eca580f5b3d7d0949bcce6fb84
Santiago Guevara <sguevaraidarraga@gmail.com>
2025-02-25 08:19:35 -0500

    solved

--- Tarea 3/forest.py
+++ Tarea 3/forest.py
@@ -2,7 +2,7 @@
     Arboles y Grafos 2025-1
     Santiago Guevara Idarraga
     Problem B: forest
-    Febrero 24 de 2025
+    Febrero 25 de 2025
 """
 
 from sys import stdin
@@ -11,61 +11,47 @@ from heapq import heappush, heappop
 
 directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-class Tree:
-    def __init__(self, specie, height):
-        self.specie = specie
-        self.height = height
-        self.visited = False
-    def __lt__(self, b):
-        ans = False
-        if self.specie != b.specie:
-            ans = self.specie < b.specie
-        elif self.height != b.height:
-            ans = self.height < b.height
-        return ans
-    def __str__(self):
-        return f"({self.specie}, {self.height}, {self.visited})"
-    def __repr__(self):
-        return self.__str__()
-def bfsAux(g, x, y):
-	ans = g[x][y].height
+def bfsAux(g, vis, x, y):
+	ans = g[x][y][1]
 	q = deque()
+	vis[x][y] = True
 	q.append((x, y))
-	g[x][y].visited = True
-	while len(q) != 0:
-		i, j = q.popleft()
-		#print(g[i][j])
+	while len(q) > 0:
+		cx, cy = q.popleft()
 		for dx, dy in directions:
-			nx, ny = i + dx, j + dy
-			#print(nx, ny)
-			if 0 <= nx < len(g) and 0 <= ny < len(g[0]) and g[nx][ny].specie == g[x][y].specie and not g[nx][ny].visited:
+			nx, ny = cx + dx, cy + dy
+			if 0 <= nx < len(g) and 0 <= ny < len(g[0]) and not vis[nx][ny] and g[nx][ny][0] == g[cx][cy][0]:
+				vis[nx][ny] = True
 				q.append((nx, ny))
-				g[nx][ny].visited = True
-				ans = max(ans, g[nx][ny].height)
+				ans = max(ans, g[nx][ny][1])
 	return ans
 def bfs(g):
+	ans = []
+	vis = [[False for _ in range(len(g[0]))] for _ in range(len(g))]
 	for i in range(len(g)):
 		for j in range(len(g[0])):
-			if not g[i][j].visited:
-				ans = bfsAux(g, i, j)
-				print(f"{g[i][j].specie} {ans}")
+			if not vis[i][j]:
+				v = bfsAux(g, vis, i, j)
+				ans.append((g[i][j][0], v))
+	return ans
 def main():
 	cases = int(stdin.readline())
 	currCase = 1
 	while cases:
 		n, m = map(int, stdin.readline().split())
 		forest = []
-		pq = []
 		for i in range(n):
 			tmp = []
 			line = stdin.readline().split()
 			for j in line:
 				specie, height = j.split('#')
-				tmp.append(Tree(specie, height))
+				tmp.append((specie, int(height)))
 			forest.append(tmp)
 		print(f"Forest #{currCase}")
-		bfs(forest)
-		print(forest)
+		data = bfs(forest)
+		data.sort(key=lambda x: (x[0], x[1]))
+		for specie, height in data:
+			print(f"{specie} {height}")
 		currCase += 1
 		cases -= 1
 main()
